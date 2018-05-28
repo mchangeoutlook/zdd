fcfb86ebeb73e01530ab33fefb7bf17111d3b2dd
mido <mido@10.0.0.9>
2018-05-28 12:39:32 +0800

    rename

--- bigcommonutil/src/com/zdd/bdc/config/ConfigClient.java
+++ bigcommonutil/src/com/zdd/bdc/config/ConfigClient.java
@@ -1,5 +1,5 @@
 package com.zdd.bdc.config;
 
-public class Client {
+public class ConfigClient {
 
 }
--- bigcommonutil/src/com/zdd/bdc/config/ConfigServer.java
+++ bigcommonutil/src/com/zdd/bdc/config/ConfigServer.java
@@ -1,5 +1,5 @@
 package com.zdd.bdc.config;
 
-public class Server {
+public class ConfigServer {
 
 }
