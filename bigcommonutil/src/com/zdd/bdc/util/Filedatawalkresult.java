bbafb8d583893c5adbbdd5fb9eb27fad75373670
mido <mido@192.168.3.56>
2018-10-22 13:17:20 +0800

    new file content format

---
+++ bigcommonutil/src/com/zdd/bdc/util/Filedatawalkresult.java
@@ -0,0 +1,39 @@
+package com.zdd.bdc.util;
+
+public class Filedatawalkresult {
+	private int walkaction = -1;
+	private int dataction = -1;
+	
+	private byte[] value1 = null;
+	private byte[] value2 = null;
+	
+	public Filedatawalkresult(int thewalkaction, int thedataction, byte[] v1, byte[] v2) {
+		walkaction = thewalkaction;
+		dataction = thedataction;
+		value1 = v1;
+		value2 = v2;
+	}
+	
+	public byte[] getvalue1() {
+		return value1;
+	}
+	public byte[] getvalue2() {
+		return value2;
+	}
+	
+	public int getwalkaction() {
+		return walkaction;
+	}
+	public int getdataction() {
+		return dataction;
+	}
+	
+	
+	public static final int WALK_CONTINUE = 0;
+	public static final int WALK_TERMINATE = 1;
+
+	public static final int DATA_DELETE = 0;
+	public static final int DATA_REPLACE = 1;
+	public static final int DATA_DONOTHING = -1;
+	
+}
