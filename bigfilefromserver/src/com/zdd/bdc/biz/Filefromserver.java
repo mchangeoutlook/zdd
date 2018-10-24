1c03a5e6df5299ab011c11b094a45346f5bac9c9
mido <mido@192.168.3.56>
2018-10-24 14:38:15 +0800

    repackage

---
+++ bigfilefromserver/src/com/zdd/bdc/biz/Filefromserver.java
@@ -0,0 +1,43 @@
+package com.zdd.bdc.biz;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import com.zdd.bdc.ex.Theserverprocess;
+import com.zdd.bdc.util.STATIC;
+
+public class Filefromserver implements Theserverprocess {
+	
+	private Path targetpath;
+	
+	@Override
+	public void init(int bigfilehash) {
+
+	}
+
+	@Override
+	public void request(byte[] b) throws Exception {
+		String path = new String(b,STATIC.CHARSET_DEFAULT);
+		if (path.startsWith("/")) {
+			path = path.replaceFirst("/", "");
+		}
+		targetpath = STATIC.LOCAL_DATAFOLDER.resolve(path);
+	}
+
+	@Override
+	public void requests(byte[] b) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public byte[] response() throws Exception {
+		return "done".getBytes();
+	}
+
+	@Override
+	public InputStream responses() throws Exception {
+		return Files.newInputStream(targetpath);
+	}
+
+}
---
+++ bigfilefromserver/src/com/zdd/bdc/main/Starter.java
@@ -0,0 +1,70 @@
+package com.zdd.bdc.main;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Date;
+import java.util.Enumeration;
+
+import com.zdd.bdc.biz.Configclient;
+import com.zdd.bdc.biz.Filefromserver;
+import com.zdd.bdc.ex.Theserver;
+import com.zdd.bdc.util.STATIC;
+
+/**
+ * @author mido how to run: nohup /data/jdk-9.0.4/bin/java -cp bigfilefromserver.jar:../../commonlibs/bigcommonutil.jar:../../commonlibs/bigexclient.jar:../../commonlibs/bigconfigclient.jar:../../commonlibs/bigexserver.jar com.zdd.bdc.main.Starter pngbigfrom > log.runbigfilefromserver &
+ */
+
+public class Starter {
+	public static void main(String[] s) throws Exception {
+		String localip = null;
+		Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+		while (en.hasMoreElements()) {
+			NetworkInterface i = (NetworkInterface) en.nextElement();
+			for (Enumeration<InetAddress> en2 = i.getInetAddresses(); en2.hasMoreElements();) {
+				InetAddress addr = (InetAddress) en2.nextElement();
+				if (!addr.isLoopbackAddress()) {
+					if (addr instanceof Inet4Address) {
+						localip = addr.getHostName();
+						break;
+					}
+				}
+			}
+		}
+		if (localip == null) {
+			localip = InetAddress.getLocalHost().getHostAddress();
+		}
+		final String ip = localip;
+
+		final String port = Configclient.getinstance(s[0], STATIC.REMOTE_CONFIGFILE_BIGDATA).read(STATIC.splitenc(STATIC.PARENTFOLDER, ip));
+		
+		final StringBuffer pending = new StringBuffer();
+		
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					Theserver.startblocking(ip, Integer.parseInt(port), STATIC.REMOTE_CONFIGVAL_PENDING, pending, 10, Filefromserver.class);
+				} catch (Exception e) {
+					System.out.println(new Date() + " ==== System exit due to below exception:");
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}
+
+		}).start();
+
+		while (!STATIC.REMOTE_CONFIGVAL_PENDING.equals(Configclient.getinstance(STATIC.NAMESPACE_CORE, STATIC.REMOTE_CONFIGFILE_PENDING).read(STATIC.splitenc(ip, port)))) {
+			try {
+				Thread.sleep(30000);
+			} catch (InterruptedException e) {
+				// do nothing
+			}
+		}
+		pending.append(STATIC.REMOTE_CONFIGVAL_PENDING);
+		System.out.println(new Date() + " ==== System will exit when next connection attempts.");
+
+	}
+
+}
