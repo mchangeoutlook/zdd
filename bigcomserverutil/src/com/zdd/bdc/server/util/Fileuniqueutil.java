f1e2a537fc3873ecad1b17b890e46cf774b079b9
mido <mido@10.0.0.5>
2018-12-30 21:37:10 +0800

    adding unique

---
+++ bigcomserverutil/src/com/zdd/bdc/server/util/Fileuniqueutil.java
@@ -0,0 +1,298 @@
+package com.zdd.bdc.server.util;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.UUID;
+import com.zdd.bdc.client.util.STATIC;
+
+public class Fileuniqueutil {
+
+	private static final Path uniquerootfolder = Paths.get(".");// STATIC.LOCAL_DATAFOLDER;
+	private static final int capacitykeymax = 60;
+	private static final int capacityvalue = 40;
+	private static final int[] rootrange = { 8, 16 };
+	private static final long leafmax = Long.parseLong(
+			"1" + String.format("%0" + (String.valueOf(Long.MAX_VALUE).length() - rootrange[1]) + "d", 0)) - 1;
+
+	private static long hashkey(byte[] key) {
+		return Math.abs(UUID.nameUUIDFromBytes(key).getLeastSignificantBits());
+	}
+
+	private static long hashleaf(byte[] key) {
+		return Long.parseLong(String.valueOf(hashkey(key)).substring(rootrange[0], rootrange[1]));
+	}
+
+	private static long hashvalue(byte[] key, int distributions) throws Exception {
+		if (distributions <= 0) {
+			throw new Exception("distributionsnegative");
+		}
+		if (distributions > leafmax) {
+			throw new Exception("distributexceed" + leafmax);
+		}
+
+		long leaf = Long.parseLong(String.valueOf(hashkey(key)).substring(rootrange[1]));
+		long leafseg = leafmax / distributions;
+		return leaf - leafseg * (leaf / leafseg);
+	}
+
+	private static Path uniquefolder() throws Exception {
+		Path returnvalue = uniquerootfolder.resolve("#unique#");
+		if (!Files.exists(returnvalue)) {
+			Files.createDirectories(returnvalue);
+		}
+		return returnvalue;
+	}
+
+	private static Path rootfile(String filter) throws Exception {
+		Path returnvalue = uniquefolder().resolve(filterfolder(filter)).resolve("root");
+		if (!Files.exists(returnvalue.getParent())) {
+			Files.createDirectories(returnvalue.getParent());
+		}
+		if (!Files.exists(returnvalue)) {
+			Files.write(returnvalue, new byte[0], StandardOpenOption.CREATE);
+		}
+		return returnvalue;
+	}
+
+	private static String filterfolder(String filter) {
+		String filterfolder = STATIC.urlencode(filter);
+		if (filterfolder == null || filterfolder.trim().isEmpty()) {
+			filterfolder = "defaultfilter";
+		}
+		return filterfolder;
+	}
+
+	private static Path leafolder(String filter) throws Exception {
+		Path returnvalue = uniquefolder().resolve(filterfolder(filter)).resolve("leaf");
+		if (!Files.exists(returnvalue)) {
+			Files.createDirectories(returnvalue);
+		}
+		return returnvalue;
+	}
+
+	private static Path collisionfolder(String filter) throws Exception {
+		Path returnvalue = uniquefolder().resolve(filterfolder(filter)).resolve("collision");
+		if (!Files.exists(returnvalue)) {
+			Files.createDirectories(returnvalue);
+		}
+		return returnvalue;
+	}
+
+	private static ByteBuffer formatkeyvalue(byte[] key, byte[] value) throws Exception {
+		if (key == null || key.length == 0) {
+			throw new Exception("nokey");
+		}
+		if (key.length > capacitykeymax) {
+			throw new Exception("keyexceed" + capacitykeymax);
+		}
+		if (key[0] == 0) {
+			throw new Exception("invalidkey");
+		}
+		if (value == null || value.length == 0) {
+			throw new Exception("novalue");
+		}
+		if (value.length != capacityvalue) {
+			throw new Exception("valuenot" + capacityvalue);
+		}
+		if (value[0] == 0) {
+			throw new Exception("invalidvalue");
+		}
+		ByteBuffer returnvalue = ByteBuffer.allocate(capacityvalue + capacitykeymax);
+		returnvalue.put(value);
+		returnvalue.put(key);
+		if (key.length < capacitykeymax) {
+			returnvalue.put(new byte[capacitykeymax - key.length]);
+		}
+		return returnvalue;
+	}
+
+	private static void collision(Path target, byte[] key, ByteBuffer towrite, ByteBuffer toread) throws Exception {
+		if (!Files.exists(target)) {
+			Files.write(target, new byte[0], StandardOpenOption.CREATE);
+		}
+		SeekableByteChannel sbc = null;
+		try {
+			sbc = Files.newByteChannel(target, StandardOpenOption.WRITE, StandardOpenOption.READ,
+					StandardOpenOption.SYNC);
+			long position = 0;
+			sbc.position(position);
+
+			ByteBuffer read = ByteBuffer.allocate(capacityvalue + capacitykeymax);
+			sbc.read(read);
+			byte[] r = read.array();
+			byte[] t = null;
+			if (towrite != null) {
+				t = towrite.array();
+			}
+			while (r[0] != 0) {
+				if (t != null) {
+					boolean duplicate = true;
+					for (int i = capacityvalue; i < capacityvalue + capacitykeymax; i++) {
+						if (t[i] != r[i]) {
+							duplicate = false;
+							break;
+						}
+					}
+					if (duplicate) {
+						throw new Exception("duplicate");
+					}
+				} else {
+					boolean samekey = true;
+					for (int i = 0; i < key.length; i++) {
+						if (key[i] != r[capacityvalue + i]) {
+							samekey = false;
+							break;
+						}
+					}
+					if (samekey) {
+						toread.put(Arrays.copyOf(r, capacityvalue));
+						break;
+					}
+				}
+				read = ByteBuffer.allocate(capacityvalue + capacitykeymax);
+				sbc.read(read);
+				r = read.array();
+			}
+			if (t != null) {
+				towrite.flip();
+				sbc.write(towrite);
+			}
+		} finally {
+			if (sbc != null) {
+				sbc.close();
+			}
+		}
+	}
+
+	public static void create(String filter, byte[] key, byte[] value, int distributions) throws Exception {
+		ByteBuffer writeleaf = formatkeyvalue(key, value);
+		synchronized (Fileutil.synckey(String.valueOf(hashkey(key)))) {
+			ByteBuffer root = ByteBuffer.allocate(1);
+			long rootstartposition = hashleaf(key) * 32;
+			Fileutil.read(rootfile(filter), rootstartposition, root);
+			if (root.array()[0] == 0) {
+				String leafile = UUID.randomUUID().toString().replaceAll("-", "");
+				ByteBuffer writeroot = ByteBuffer.allocate(32);
+				writeroot.put(STATIC.tobytes(leafile));
+				Fileutil.write(rootfile(filter), rootstartposition, writeroot);
+				Fileutil.write(leafolder(filter).resolve(leafile),
+						hashvalue(key, distributions) * (capacityvalue + capacitykeymax), writeleaf);
+			} else {
+				ByteBuffer readleafile = ByteBuffer.allocate(32);
+				Fileutil.read(rootfile(filter), rootstartposition, readleafile);
+				String leafile = STATIC.tostring(readleafile.array());
+				if (!Files.exists(leafolder(filter).resolve(leafile))) {
+					Fileutil.write(leafolder(filter).resolve(leafile),
+							hashvalue(key, distributions) * (capacityvalue + capacitykeymax), writeleaf);
+				} else {
+					ByteBuffer leaf = ByteBuffer.allocate(1);
+					long leafstartposition = hashvalue(key, distributions) * (capacityvalue + capacitykeymax);
+					Fileutil.read(leafolder(filter).resolve(leafile), leafstartposition, leaf);
+					if (leaf.array()[0] == 0) {
+						Fileutil.write(leafolder(filter).resolve(leafile),
+								hashvalue(key, distributions) * (capacityvalue + capacitykeymax), writeleaf);
+					} else {
+						ByteBuffer readvalue = ByteBuffer.allocate(capacityvalue + capacitykeymax);
+						Fileutil.read(leafolder(filter).resolve(leafile), leafstartposition, readvalue);
+						boolean duplicate = true;
+						byte[] val = readvalue.array();
+						for (int i = 0; i < key.length; i++) {
+							if (key[i] != val[capacityvalue + i]) {
+								duplicate = false;
+								break;
+							}
+						}
+						if (duplicate) {
+							throw new Exception("duplicate");
+						} else {
+							if (val[32] == 0) {
+								String collisionfile = STATIC.tostring(Arrays.copyOf(val, 32));
+								if (!Files.exists(collisionfolder(filter).resolve(collisionfile))) {
+									Fileutil.write(leafolder(filter).resolve(leafile),
+											hashvalue(key, distributions) * (capacityvalue + capacitykeymax),
+											writeleaf);
+								} else {
+									collision(collisionfolder(filter).resolve(collisionfile), null, writeleaf, null);
+								}
+							} else {
+								String collisionfile = UUID.randomUUID().toString().replaceAll("-", "");
+								ByteBuffer writeleafcollision = ByteBuffer.allocate(capacityvalue + capacitykeymax);
+								writeleafcollision.put(STATIC.tobytes(collisionfile));
+								writeleafcollision.put(new byte[capacityvalue + capacitykeymax - 32]);
+								Fileutil.write(leafolder(filter).resolve(leafile),
+										hashvalue(key, distributions) * (capacityvalue + capacitykeymax),
+										writeleafcollision);
+
+								ByteBuffer writecollision = ByteBuffer.allocate(2 * (capacityvalue + capacitykeymax));
+								writecollision.put(val);
+								writecollision.put(writeleaf.array());
+								Fileutil.write(collisionfolder(filter).resolve(collisionfile), 0, writecollision);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public static byte[] read(String filter, byte[] key, int distributions) throws Exception {
+		ByteBuffer root = ByteBuffer.allocate(1);
+		long rootstartposition = hashleaf(key) * 32;
+		Fileutil.read(rootfile(filter), rootstartposition, root);
+		if (root.array()[0] == 0) {
+			return null;
+		} else {
+			ByteBuffer readleafile = ByteBuffer.allocate(32);
+			Fileutil.read(rootfile(filter), rootstartposition, readleafile);
+			String leafile = STATIC.tostring(readleafile.array());
+			if (!Files.exists(leafolder(filter).resolve(leafile))) {
+				return null;
+			} else {
+				ByteBuffer leaf = ByteBuffer.allocate(1);
+				long leafstartposition = hashvalue(key, distributions) * (capacityvalue + capacitykeymax);
+				Fileutil.read(leafolder(filter).resolve(leafile), leafstartposition, leaf);
+				if (leaf.array()[0] == 0) {
+					return null;
+				} else {
+					ByteBuffer readvalue = ByteBuffer.allocate(capacityvalue + capacitykeymax);
+					Fileutil.read(leafolder(filter).resolve(leafile), leafstartposition, readvalue);
+					boolean samekey = true;
+					byte[] val = readvalue.array();
+					for (int i = 0; i < key.length; i++) {
+						if (key[i] != val[capacityvalue + i]) {
+							samekey = false;
+							break;
+						}
+					}
+					if (samekey) {
+						return Arrays.copyOf(val, capacityvalue);
+					} else {
+						if (val[32] == 0) {
+							String collisionfile = STATIC.tostring(Arrays.copyOf(val, 32));
+							if (!Files.exists(collisionfolder(filter).resolve(collisionfile))) {
+								return null;
+							} else {
+								ByteBuffer returnvalue = ByteBuffer.allocate(capacityvalue);
+								collision(collisionfolder(filter).resolve(collisionfile), key, null, returnvalue);
+								byte[] rb = returnvalue.array();
+								if (rb[0] == 0) {
+									return null;
+								} else {
+									return rb;
+								}
+							}
+						} else {
+							return null;
+						}
+					}
+				}
+			}
+		}
+	}
+
+}
---
+++ bigcomserverutil/src/com/zdd/bdc/server/util/Fileutilolder.java
@@ -0,0 +1,380 @@
+package com.zdd.bdc.server.util;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.Vector;
+
+import com.zdd.bdc.client.util.STATIC;
+
+//file content format: value1maxlength(10)value1(value1000)value1length(11)value1maxlength(10)value2maxlength(10)value2(value2000)value2length(11)value2maxlength(10)
+
+public class Fileutil {
+
+	private static void formatdatahalf(ByteBuffer bb, byte[] value, int valuemaxlength, boolean isvaluedeleted) {
+		bb.put(String.format("%010d", valuemaxlength).getBytes());
+		bb.put(value);
+		bb.put(new byte[valuemaxlength - value.length]);
+		if (isvaluedeleted) {
+			bb.put(("-" + String.format("%010d", value.length)).getBytes());
+		} else {
+			bb.put(String.format("%011d", value.length).getBytes());
+		}
+		bb.put(String.format("%010d", valuemaxlength).getBytes());
+	}
+
+	private static ByteBuffer formatdatapair(byte[] value1, int value1maxlength, boolean isvalue1deleted, byte[] value2,
+			int value2maxlength, boolean isvalue2deleted) {
+		ByteBuffer bb = ByteBuffer.allocate(10 + value1maxlength + 11 + 10 + 10 + value2maxlength + 11 + 10);
+		formatdatahalf(bb, value1, value1maxlength, isvalue1deleted);
+		formatdatahalf(bb, value2, value2maxlength, isvalue2deleted);
+		return bb;
+	}
+
+	// return Vector[valuemaxlength,value,valuelength]
+	private static Vector<byte[]> parsedatahalf(SeekableByteChannel sbc, boolean reverse) throws Exception {
+		Vector<byte[]> returnvalue = new Vector<byte[]>(3);
+		byte[] valuemaxlength = null;
+		byte[] value = null;
+		byte[] valuelength = null;
+		if (reverse) {
+			sbc.position(sbc.position() - 10);
+			ByteBuffer bb = ByteBuffer.allocate(10);
+			sbc.read(bb);
+			valuemaxlength = bb.array();
+
+			int valuemaxl = Integer.parseInt(new String(valuemaxlength));
+			sbc.position(sbc.position() - 10 - 11);
+			bb = ByteBuffer.allocate(11);
+			sbc.read(bb);
+			valuelength = bb.array();
+
+			int valuel = Math.abs(Integer.parseInt(new String(valuelength)));
+			sbc.position(sbc.position() - 11 - valuemaxl);
+			bb = ByteBuffer.allocate(valuel);
+			sbc.read(bb);
+			value = bb.array();
+
+			sbc.position(sbc.position() - valuel - 10);
+		} else {
+			sbc.position(sbc.position());
+			ByteBuffer bb = ByteBuffer.allocate(10);
+			sbc.read(bb);
+			valuemaxlength = bb.array();
+			int valuemaxl = Integer.parseInt(new String(valuemaxlength));
+			sbc.position(sbc.position() + valuemaxl);
+			bb = ByteBuffer.allocate(11);
+			sbc.read(bb);
+			valuelength = bb.array();
+
+			int valuel = Math.abs(Integer.parseInt(new String(valuelength)));
+			sbc.position(sbc.position() - 11 - valuemaxl);
+			bb = ByteBuffer.allocate(valuel);
+			sbc.read(bb);
+			value = bb.array();
+
+			sbc.position(sbc.position() - valuel + valuemaxl + 11 + 10);
+		}
+		returnvalue.add(valuemaxlength);
+		returnvalue.add(value);
+		returnvalue.add(valuelength);
+
+		return returnvalue;
+	}
+
+	// return
+	// Vector[value1maxlength,value1,value1length,value2maxlength,value2,value2length]
+	private static Vector<byte[]> parsedatapair(SeekableByteChannel sbc, boolean reverse) throws Exception {
+		if (reverse && sbc.position() == 0 || !reverse && sbc.position() == sbc.size()) {
+			return null;
+		} else {
+			Vector<byte[]> returnvalue = new Vector<byte[]>(6);
+			Vector<byte[]> firsthalf = null;
+			Vector<byte[]> secondhalf = null;
+			if (reverse) {
+				secondhalf = parsedatahalf(sbc, reverse);
+				firsthalf = parsedatahalf(sbc, reverse);
+			} else {
+				firsthalf = parsedatahalf(sbc, reverse);
+				secondhalf = parsedatahalf(sbc, reverse);
+			}
+			returnvalue.addAll(firsthalf);
+			returnvalue.addAll(secondhalf);
+			return returnvalue;
+		}
+	}
+
+	public static void walkdata(Path target, Filedatawalk walker, boolean reverse) throws Exception {
+		if (!Files.exists(target)) {
+			// do nothing
+		} else {
+			SeekableByteChannel sbc = null;
+			try {
+				sbc = Files.newByteChannel(target, StandardOpenOption.WRITE, StandardOpenOption.READ,
+						StandardOpenOption.SYNC);
+				if (reverse) {
+					sbc.position(Files.size(target));
+				} else {
+					sbc.position(0);
+				}
+				Vector<byte[]> datapair = parsedatapair(sbc, reverse);
+				long datasequence = 0;
+				long dataseqincludedeleted = 0;
+				while (datapair != null) {
+					byte[] value1maxlength = datapair.get(0);
+					byte[] value1 = datapair.get(1);
+					byte[] value1length = datapair.get(2);
+					boolean isvalue1deleted = Integer.parseInt(new String(value1length)) < 0;
+
+					byte[] value2maxlength = datapair.get(3);
+					byte[] value2 = datapair.get(4);
+					byte[] value2length = datapair.get(5);
+					boolean isvalue2deleted = Integer.parseInt(new String(value2length)) < 0;
+
+					Filedatawalkresult res = walker.data(datasequence, dataseqincludedeleted, value1, isvalue1deleted, value2, isvalue2deleted);
+					if (res == null) {// move to next data
+						datapair = parsedatapair(sbc, reverse);
+						dataseqincludedeleted++;
+						if (!isvalue1deleted&&!isvalue2deleted) {
+							datasequence++;
+						}
+					} else {
+						if (res.getdataction() == Filedatawalkresult.DATA_DELETE) {
+							ByteBuffer towrite = formatdatapair(value1, Integer.parseInt(new String(value1maxlength)),
+									true, value2, Integer.parseInt(new String(value2maxlength)), true);
+							if (reverse) {
+								// keep current position;
+							} else {
+								sbc.position(sbc.position() - towrite.capacity());
+							}
+							synchronized (STATIC.synckey(new String(value1))) {
+								write(sbc, towrite);
+							}
+							if (reverse) {
+								sbc.position(sbc.position() - towrite.capacity());
+							} else {
+								// keep current position;
+							}
+						} else if (res.getdataction() == Filedatawalkresult.DATA_REPLACE) {
+							ByteBuffer towrite = formatdatapair(res.getvalue1(),
+									Integer.parseInt(new String(value1maxlength)), false, res.getvalue2(),
+									Integer.parseInt(new String(value2maxlength)), false);
+							if (reverse) {
+								// keep current position;
+							} else {
+								sbc.position(sbc.position() - towrite.capacity());
+							}
+							String sync = null;
+							if (Arrays.equals(res.getvalue1(), value1)) {
+								sync = new String(value1);
+							} else {
+								sync = new String(value2);
+							}
+							synchronized (STATIC.synckey(sync)) {
+								write(sbc, towrite);
+							}
+							if (reverse) {
+								sbc.position(sbc.position() - towrite.capacity());
+							} else {
+								// keep current position;
+							}
+						} else {
+							// do nothing to data
+						}
+
+						if (res.getwalkaction() == Filedatawalkresult.WALK_CONTINUE) {
+							datapair = parsedatapair(sbc, reverse);
+							dataseqincludedeleted++;
+							if (!isvalue1deleted&&!isvalue2deleted) {
+								datasequence++;
+							}
+						} else {
+							break;
+						}
+					}
+				}
+
+			} finally {
+				if (sbc != null) {
+					sbc.close();
+				}
+			}
+		}
+	}
+
+	private static void write(Path target, long startposition, ByteBuffer towrite) throws Exception {
+		if (!Files.exists(target) && target.getParent() != null && !Files.exists(target.getParent())) {
+			Files.createDirectories(target.getParent());
+		}
+		SeekableByteChannel sbc = null;
+		try {
+			sbc = Files.newByteChannel(target, StandardOpenOption.CREATE, StandardOpenOption.WRITE,
+					StandardOpenOption.SYNC);
+			sbc.position(startposition);
+			write(sbc, towrite);
+		} finally {
+			if (sbc != null) {
+				sbc.close();
+			}
+		}
+	}
+
+	private static void write(SeekableByteChannel sbc, ByteBuffer towrite) throws Exception {
+		towrite.flip();
+		sbc.write(towrite);
+	}
+
+	public static void create(byte[] value1, int value1maxlength, byte[] value2, int value2maxlength, Path target)
+			throws Exception {
+		ByteBuffer towrite = formatdatapair(value1, value1maxlength, false, value2, value2maxlength, false);
+		synchronized (STATIC.syncfile(target)) {
+			if (Files.exists(target)) {
+				write(target, Files.size(target), towrite);
+			} else {
+				write(target, 0, towrite);
+			}
+		}
+	}
+
+	public static void modifylastvalue2byvalue1(byte[] value1, byte[] newvalue2, Path target) throws Exception {
+		walkdata(target, new Filedatawalk() {
+
+			@Override
+			public Filedatawalkresult data(long datasequence, long dataseqincludedeleted, byte[] v1, boolean isv1deleted, byte[] v2, boolean isv2deleted) {
+				if (isv1deleted || isv2deleted) {
+					return null;
+				} else {
+					if (Arrays.equals(value1, v1)) {
+						return new Filedatawalkresult(Filedatawalkresult.WALK_TERMINATE,
+								Filedatawalkresult.DATA_REPLACE, value1, newvalue2);
+					} else {
+						// ignore the data
+						return null;
+					}
+				}
+			}
+
+		}, true);
+	}
+
+	public static void deletelastvalue2byvalue1(byte[] value1, Path target) throws Exception {
+		walkdata(target, new Filedatawalk() {
+
+			@Override
+			public Filedatawalkresult data(long datasequence, long dataseqincludedeleted, byte[] v1, boolean isv1deleted, byte[] v2, boolean isv2deleted) {
+				if (isv1deleted || isv2deleted) {
+					return null;
+				} else {
+					if (Arrays.equals(value1, v1)) {
+						return new Filedatawalkresult(Filedatawalkresult.WALK_TERMINATE, Filedatawalkresult.DATA_DELETE,
+								null, null);
+					} else {
+						// ignore the data
+						return null;
+					}
+				}
+			}
+
+		}, true);
+	}
+
+	public static Vector<byte[]> readfirstvalue1value2(Path target) throws Exception {
+		Vector<byte[]> returnvalue = new Vector<byte[]>(2);
+		walkdata(target, new Filedatawalk() {
+
+			@Override
+			public Filedatawalkresult data(long datasequence, long dataseqincludedeleted, byte[] v1, boolean isv1deleted, byte[] v2, boolean isv2deleted) {
+				returnvalue.add(v1);
+				returnvalue.add(v2);
+				return new Filedatawalkresult(Filedatawalkresult.WALK_TERMINATE, Filedatawalkresult.DATA_DONOTHING,
+						null, null);
+			}
+
+		}, false);
+		return returnvalue;
+	}
+
+	public static byte[] readfirstvalue2byvalue1(byte[] value1, Path target) throws Exception {
+		Vector<byte[]> returnvalue = new Vector<byte[]>(1);
+		walkdata(target, new Filedatawalk() {
+
+			@Override
+			public Filedatawalkresult data(long datasequence, long dataseqincludedeleted, byte[] v1, boolean isv1deleted, byte[] v2, boolean isv2deleted) {
+				if (isv1deleted || isv2deleted) {
+					return null;
+				} else {
+					if (Arrays.equals(value1, v1)) {
+						returnvalue.add(v2);
+						return new Filedatawalkresult(Filedatawalkresult.WALK_TERMINATE,
+								Filedatawalkresult.DATA_DONOTHING, null, null);
+					} else {
+						// ignore the data
+						return null;
+					}
+				}
+			}
+
+		}, false);
+		if (returnvalue.isEmpty()) {
+			return null;
+		} else {
+			return returnvalue.get(0);
+		}
+	}
+
+	public static byte[] readlastvalue2byvalue1(byte[] value1, Path target) throws Exception {
+		Vector<byte[]> returnvalue = new Vector<byte[]>(1);
+		walkdata(target, new Filedatawalk() {
+
+			@Override
+			public Filedatawalkresult data(long datasequence, long dataseqincludedeleted, byte[] v1, boolean isv1deleted, byte[] v2, boolean isv2deleted) {
+				if (isv1deleted || isv2deleted) {
+					return null;
+				} else {
+					if (Arrays.equals(value1, v1)) {
+						returnvalue.add(v2);
+						return new Filedatawalkresult(Filedatawalkresult.WALK_TERMINATE,
+								Filedatawalkresult.DATA_DONOTHING, null, null);
+					} else {
+						// ignore the data
+						return null;
+					}
+				}
+			}
+
+		}, true);
+		if (returnvalue.isEmpty()) {
+			return null;
+		} else {
+			return returnvalue.get(0);
+		}
+
+	}
+
+	public static Vector<byte[]> readallvalue2byvalue1(byte[] value1, int defaultnumofvalues, boolean reverse,
+			Path target) throws Exception {
+		Vector<byte[]> returnvalue = new Vector<byte[]>(defaultnumofvalues);
+		walkdata(target, new Filedatawalk() {
+
+			@Override
+			public Filedatawalkresult data(long datasequence, long dataseqincludedeleted, byte[] v1, boolean isv1deleted, byte[] v2, boolean isv2deleted) {
+				if (isv1deleted || isv2deleted) {
+					return null;
+				} else {
+					if (Arrays.equals(value1, v1)) {
+						returnvalue.add(v2);
+					} else {
+						// ignore the data
+					}
+					return null;
+				}
+			}
+
+		}, reverse);
+		return returnvalue;
+	}
+
+}
