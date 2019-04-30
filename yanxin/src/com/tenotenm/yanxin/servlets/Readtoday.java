7b1ea43b07c80b1bb26e0facc6f490aa37426168
mido <mido@192.168.3.104>
2019-04-30 20:47:35 +0800

    doing htm

---
+++ yanxin/src/com/tenotenm/yanxin/servlets/Readtoday.java
@@ -0,0 +1,34 @@
+package com.tenotenm.yanxin.servlets;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tenotenm.yanxin.entities.Yxaccount;
+import com.tenotenm.yanxin.util.Bizutil;
+import com.tenotenm.yanxin.util.Reuse;
+
+@SuppressWarnings("serial")
+@WebServlet("/check/readtoday")
+public class Readtoday extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			Yxaccount yxaccount = (Yxaccount)request.getAttribute(Yxaccount.class.getSimpleName());
+			Date today = new Date();
+			Map<String, String> ret = Bizutil.convert(Bizutil.readyanxin(yxaccount, today));
+			ret.put("today", Reuse.yyyyMMdd(today));
+			Reuse.respond(response, ret, null);
+
+		} catch (Exception e) {
+			Reuse.respond(response, null, e);
+		}
+	}
+}
